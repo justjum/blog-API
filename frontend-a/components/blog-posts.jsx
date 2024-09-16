7cded66710c220436f92f9e393cb6fc6e216b651
justjum <117821093+justjum@users.noreply.github.com>
2024-09-17 09:05:54 +0930

    Add focused post - Might be better as a component. - Definitely would be better as a component        "<Post />"

--- frontend-a/components/blog-posts.jsx
+++ frontend-a/components/blog-posts.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 
 export default function BlogPosts() {
   const [posts, setPosts] = useState(null);
+  const [focusPost, setFocusPost] = useState(null);
+  const [comments, setComments] = useState(null);
 
-  const requestOptions = {
+  let requestOptions = {
     method: "get",
     headers: {
       "Content-Type": "application/json",
@@ -21,22 +23,89 @@ export default function BlogPosts() {
     );
   }, []);
 
+  useEffect(() => {
+    focusPost
+      ? fetch(
+          `//127.0.0.1:3000/post/${focusPost.id}/comment`,
+          requestOptions
+        ).then((response) =>
+          response.json().then((data) => {
+            console.log(data);
+            setComments(data);
+          })
+        )
+      : "";
+  }, [focusPost]);
+
+  const handleFocusPost = (e) => {
+    e.preventDefault();
+    setFocusPost(posts.find((post) => post.id == e.target.id));
+  };
+
+  const handleClosePost = () => {
+    setFocusPost(null);
+  };
+
   return (
     <>
-      <ul>
-        {posts
-          ? posts.map((post) => (
-              <div className="post-card" key={post.id}>
-                <div className="card-title">
-                  <h3>{post.title}</h3>
-                </div>
-                <div className="card-body">
-                  <p>{post.text}</p>
-                </div>
-              </div>
-            ))
-          : ""}
-      </ul>
+      {focusPost ? (
+        <>
+          <div className="post-card center" key={focusPost.id}>
+            <div className="card-title">
+              <h3>{focusPost.title}</h3>
+            </div>
+            <div className="card-body">
+              <p>{focusPost.text}</p>
+            </div>
+            <div className="card-comments">
+              {comments
+                ? comments.map((comment) => {
+                    return (
+                      <>
+                        <p>{comment.text}</p>
+                        <p>{comment.userId}</p>
+                        <p>{comment.createdAt}</p>
+                      </>
+                    );
+                  })
+                : ""}
+            </div>
+            <div>
+              <button className="formInput" onClick={handleClosePost}>
+                Back
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <section>
+          {posts
+            ? posts.map((post) => {
+                return post.published ? (
+                  <div className="post-card center" key={post.id}>
+                    <div className="card-title">
+                      <h3>{post.title}</h3>
+                    </div>
+                    <div className="card-body">
+                      <p>{post.text}</p>
+                    </div>
+                    <div>
+                      <button
+                        className="formInput"
+                        id={post.id}
+                        onClick={handleFocusPost}
+                      >
+                        Full Post
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                );
+              })
+            : ""}
+        </section>
+      )}
     </>
   );
 }
